f84b880fd268bc61181678314d1270bcd05b1100
secuflag <secuflag@ticino.com>
2019-10-20 15:57:04 +0200

    Remove unneeded ATV settings
     * Already defined in device/google/atv

--- software.mk
+++ software.mk
@@ -6,31 +6,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.cts.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.cts.xml \
     frameworks/native/data/etc/android.software.voice_recognizers.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.voice_recognizers.xml
 
-ifeq ($(TARGET_BUILD_GOOGLE_ATV), true)
-PRODUCT_COPY_FILES += \
-    device/hardkernel/common/android.software.google_atv.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.google_atv.xml
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.live_tv.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.live_tv.xml
-PRODUCT_PACKAGE_OVERLAYS += device/hardkernel/common/atv_gms_overlay
-PRODUCT_PACKAGES += \
-    TvProvider \
-    GooglePackageInstaller \
-    com.android.media.tv.remoteprovider.xml \
-    com.android.media.tv.remoteprovider
-$(call add-clean-step, rm -rf $(OUT_DIR)/vendor/etc/permissions/android.hardware.camera.front.xml)
-
-else
-#PRODUCT_PACKAGE_OVERLAYS += device/hardkernel/common/aosp_gms_overlay
-#PRODUCT_PACKAGES += \
-#    QuickSearchBox \
-#    Contacts \
-#    Calendar \
-#    BlockedNumberProvider \
-#    BookmarkProvider \
-#    MtpDocumentsProvider \
-#    DownloadProviderUi
-endif
-
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.app.rotation=original \
     media.amplayer.widevineenable=true
